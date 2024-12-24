372448794d4475e53fa887015fa4faf79a077051
Refaat Hassan <refathassan1289641@gmail.com>
2024-12-25 01:53:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+radius=float(input("please enter the radius :   "))
+circumference=2*math.pi*radius
+area=math.pi*(radius**2)
+print("the circumference  ",circumference)
+print("the area  ",area)
